88ba7022a3ec2e8e2624b7587e6cd3d5492649c0
Christian Bonilla <christianurielbonillasuarez@gmail.com>
2025-02-01 21:40:49 -0800

    Class 1

---
+++ src/components/footer.jsx
@@ -0,0 +1,11 @@
+import './footer.css';
+
+function Footer() {
+    return (
+        <footer>
+            <p>Created by: Christian Bonilla</p>
+        </footer>
+    );
+}
+
+export default Footer;
