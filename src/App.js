dfc29b74f5ba45670bb779847e3fd6d2cfcf6e8f
Christian Bonilla <christianurielbonillasuarez@gmail.com>
2025-02-03 13:57:14 -0800

    Complete assignment 1

--- src/App.js
+++ src/App.js
@@ -1,40 +1,54 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import Navbar from './components/navbar';
 import Counter from './components/counter';
 import Footer from './components/footer';
+import CatalogPage from './components/catalogPage';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       {/* <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Lenaarn React
+//         </a>
+//       </header> */}
+//       <Navbar></Navbar>
+//       {/* <div className="content"> */}
+//       <div>
+//         <h1>
+//           Hello World from React!
+//         </h1>
+//       </div>
+//       <Counter></Counter>
+//       <Footer></Footer>
+//       {/* <h1>
+//         Hello World from React!
+//       </h1>
+//       <Footer></Footer> */}
+//     </div>
+//   );
+// }
 
 function App() {
   return (
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Lenaarn React
-        </a>
-      </header> */}
-      <Navbar></Navbar>
-      {/* <div className="content"> */}
-      <div>
-        <h1>
-          Hello World from React!
-        </h1>
+      <div className="App">
+          <Navbar />
+          <main>
+              <CatalogPage />
+          </main>
+          <Footer />
       </div>
-      <Counter></Counter>
-      <Footer></Footer>
-      {/* <h1>
-        Hello World from React!
-      </h1>
-      <Footer></Footer> */}
-    </div>
   );
 }
 
