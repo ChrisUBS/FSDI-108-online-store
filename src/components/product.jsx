14d6376fd8a3f05104bde0a37422385931194bb5
Christian Bonilla <christianurielbonillasuarez@gmail.com>
2025-02-06 21:37:23 -0800

    Class 3

--- src/components/product.jsx
+++ src/components/product.jsx
@@ -1,20 +1,26 @@
 import './product.css';
 import QuantityPicker from './quantityPicker';
 
-function Product() {
+function Product(props) {
+
+    function addToCart() {
+        console.log('Adding to cart');
+    }
+
+
     return (
         <div className="card product">
             <img
-                src="https://picsum.photos/280/200"
+                src={props.data.image}
                 className="card-img-top"
                 alt="Product"
             />
             <div className="card-body text-center">
-                <h5 className="card-title text-primary">Product Title Here</h5>
-                <p className="card-text text-muted">$100</p>
+                <h5 className="card-title text-primary">{props.data.title}</h5>
+                <p className="card-text text-muted">${props.data.price}</p>
                 <div className="d-flex justify-content-between align-items-center mt-2">
                     <QuantityPicker />
-                    <button className="btn btn-sm btn-success">Add to Cart</button>
+                    <button className="btn btn-sm btn-success" onClick={addToCart}>Add to Cart</button>
                 </div>
             </div>
         </div>
--- src/pages/catalog.jsx
+++ src/pages/catalog.jsx
@@ -1,23 +1,96 @@
 import './catalog.css';
 import Product from '../components/product';
 
+const catalog = [
+    {
+        "title": "Orange",
+        "image": "/images/orange.png",
+        "price": 12.90,
+        "category": "fruit",
+        "_id": "1"
+    },
+    {
+        "title": "Apple",
+        "image": "/images/apple.png",
+        "price": 10.90,
+        "category": "fruit",
+        "_id": "2"
+    },
+    {
+        "title": "Banana",
+        "image": "/images/banana.webp",
+        "price": 8.90,
+        "category": "fruit",
+        "_id": "3"
+    },
+    {
+        "title": "Carrot",
+        "image": "/images/carrot.webp",
+        "price": 5.90,
+        "category": "vegetable",
+        "_id": "4"
+    },
+    {
+        "title": "Cucumber",
+        "image": "/images/cucumber.png",
+        "price": 6.90,
+        "category": "vegetable",
+        "_id": "5"
+    },
+    {
+        "title": "Tomato",
+        "image": "/images/tomato.webp",
+        "price": 7.90,
+        "category": "vegetable",
+        "_id": "6"
+    },
+    {
+        "title": "Milk",
+        "image": "/images/milk.webp",
+        "price": 3.90,
+        "category": "dairy and eggs",
+        "_id": "7"
+    },
+    {
+        "title": "Egg",
+        "image": "/images/egg.webp",
+        "price": 2.90,
+        "category": "dairy and eggs",
+        "_id": "8"
+    },
+    {
+        "title": "Butter",
+        "image": "/images/butter.png",
+        "price": 4.90,
+        "category": "dairy and eggs",
+        "_id": "9"
+    },
+    {
+        "title": "corn",
+        "image": "/images/corn.webp",
+        "price": 1.90,
+        "category": "vegetable",
+        "_id": "10"
+    }
+];
+
+const categories = ["fruit", "beverages", "vegetable", "merchandise", "dairy and eggs"];
+
 function Catalog() {
     return (
-        <div className="catalog container mt-5">
+        <div className="catalog container mt-2">
             <h1 className="text-center mb-4 text-success">Check Out Our Fresh Catalog</h1>
+            <div className="filters">
+                {categories.map(category => (
+                    <button className="btn btn-sm btn-outline-secondary mb-4 me-2">{category}</button>
+                ))}
+            </div>
             <div className="row g-4">
-                <div className="col-md-4 col-lg-3">
-                    <Product />
-                </div>
-                <div className="col-md-4 col-lg-3">
-                    <Product />
-                </div>
-                <div className="col-md-4 col-lg-3">
-                    <Product />
-                </div>
-                <div className="col-md-4 col-lg-3">
-                    <Product />
-                </div>
+                {catalog.map(product => (
+                    <div className="col-md-4 col-lg-3">
+                        <Product data={product} />
+                    </div>
+                ))}
             </div>
         </div>
     );
